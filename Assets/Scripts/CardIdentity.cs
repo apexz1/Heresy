81cedecbbbfa796db2aeab97ef901a8638c5b525
wjsimon <w.j.simon@hotmail.com>
2015-07-20 11:22:24 +0200

    -

---
+++ Assets/Scripts/CardIdentity.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardIdentity : MonoBehaviour
+{
+    public int id;
+
+    public string GetName()
+    {
+        return CardLibrary.Get().GetCard(id).GetName();
+    }
+}
---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    Resolution[] resolutions;
+
+    public Transform menuPanel;
+    public GameObject buttonPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            GameObject button = (GameObject)Instantiate(buttonPrefab);
+            button.GetComponentInChildren<Text>().text = ResToString(resolutions[i]);
+            int index = i;
+            /*button.GetComponent<Button>().onClick.AddListener(
+                () => { SetResolution(index); }
+                );*/
+
+            button.transform.parent = menuPanel;
+        }
+    }
+
+    public void SetResolution(int index)
+    {
+        Screen.SetResolution(resolutions[index].width, resolutions[index].height, false);
+    }
+
+    public string ResToString(Resolution res)
+    {
+        return res.width + " x " + res.height;
+    }
+}
